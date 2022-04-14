90f2dea179836e9a30b94a61543e6be656fd955a
Liz Horn <liz.mpls@gmail.com>
2022-04-14 13:39:28 -0500

    added mo salary calc and display

--- scripts/client.js
+++ scripts/client.js
@@ -33,26 +33,38 @@ function addEmployee(){
 
 function addToTable(){
     console.log('in add to table');
+    let allSalaries = 0;
     //empty table body
     let el = $('#tbody');
     el.empty();
     //loop through employees array
     for (let i=0; i<employees.length; i++){
         //add employee to table
-        console.log(employees[i].first);
         el.append(
             `<tr><td>${employees[i].first}</td>
             <td>${employees[i].last}</td>
             <td>${employees[i].id}</td>
             <td>${employees[i].title}</td>
             <td>${employees[i].salary}</td></tr>`)
-
+        //add salary to allSalaries
+        allSalaries += Number(employees[i].salary);
     }
-    //add MONTHLY salary to total cost
-
-    //display cost on DOM
+    //calculate monthly and display cost on DOM
+    displayCost(allSalaries);
 }
 
-function displayCost(){
-    console.log('in display cost');
+function displayCost(allSalaries){
+    // console.log('allSalaries:', allSalaries);
+    let el = $('#totalMonthly');
+    el.empty();
+    //calculate monthly cost from annual and display on DOM
+    let monthly = (allSalaries/12).toFixed(2);
+    el.append(monthly);
+    // turn monthly cost display red if exceeds 20K
+    if (monthly > 20000) {
+        el.css('background-color', 'red');
+    } else {
+        el.css('background-color', 'white');
+    }
+    
 }
